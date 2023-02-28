d4636b45fdf143a99a0ab3639139197776eee371
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-03-01 01:29:23 +0200

    Adding NewBlogs To The DB

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models  # dot is meaning import from the current dir
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(engine)
 
@@ -9,10 +10,25 @@ models.Base.metadata.create_all(engine)
 
 
 app = FastAPI()
+
+
 # u can use this inner app using uvicon after installation of req.txt by using commang:
 # uvicorn blog.main:app --reload
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.post("/blog")
-def CreatePost(request: schemas.Blog):
-    return request
+def CreatePost(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(
+        title=request.title,
+        body=request.body,
+    )
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
